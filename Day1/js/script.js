451b5be5cd787c4687b015072976752f7890a7bf
demo <gayathriganga04@gmail>
2026-06-20 23:55:08 +0530

    first commit

---
+++ Day1/js/script.js
@@ -0,0 +1,61 @@
+/*1.
+
+Reverse a string without using reverse().
+
+Input: "hello"
+Output: "olleh"
+2.
+
+Find the largest number in an array.
+
+Input: [10, 20, 50, 80, 30]
+Output: 80
+3.
+
+Count the number of vowels in a string.
+
+Input: "javascript"
+Output: 3
+4.
+
+Check whether a string is a palindrome.
+
+Input: "madam"
+Output: true
+5.
+
+Find the second largest number in an array.
+
+Input: [10, 20, 50, 80, 30]
+Output: 50
+6.
+
+Remove duplicate elements from an array without using Set.
+
+Input: [1,2,2,3,4,4,5]
+Output: [1,2,3,4,5]
+7.
+
+Find the frequency of each element in an array.
+
+Input: ["apple","banana","apple","orange","apple"]
+8.
+
+Move all zeros to the end of an array.
+
+Input: [1,0,2,0,3,4,0]
+Output: [1,2,3,4,0,0,0]
+9.
+
+Find the missing number.
+
+Input: [1,2,3,5]
+Output: 4
+10.
+
+Find common elements between two arrays.
+
+arr1 = [1,2,3,4]
+arr2 = [3,4,5,6]
+
+Output: [3,4]*/
